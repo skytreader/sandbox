2340216222e5921a75d0c42c0759c98f329406e5
Chad Estioco <chad@galadriel.(none)>
2013-02-18 00:28:05 +0800

    Added meta information to models.

--- quizapp/main/admin.py
+++ quizapp/main/admin.py
@@ -1,8 +1,8 @@
-from main.models import Users, QuizCategories, Quizzes, Tags, QuizTags
+from main.models import User, QuizCategory, Quiz, Tag, QuizTag
 from django.contrib import admin
 
-admin.site.register(Users)
-admin.site.register(QuizCategories)
-admin.site.register(Quizzes)
-admin.site.register(Tags)
-admin.site.register(QuizTags)
+admin.site.register(User)
+admin.site.register(QuizCategory)
+admin.site.register(Quiz)
+admin.site.register(Tag)
+admin.site.register(QuizTag)
--- quizapp/main/models.py
+++ quizapp/main/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 # Can I create a default model and have all models subclass from that ala CI?
 
-class Users(models.Model):
+class User(models.Model):
     username = models.CharField(max_length=20)
     password = models.CharField(max_length=255)
     total_score = models.IntegerField(default=0)
     create_time = models.DateTimeField(auto_now_add=True)
     last_update = models.DateTimeField(auto_now=True, auto_now_add=True)
     # Apparently, Django can't FK with itself?
-    # last_updater = models.ForeignKey(Users)
+    # last_updater = models.ForeignKey(User)
     can_read = models.BooleanField(default=True)
     can_write = models.BooleanField(default=True)
     can_exec = models.BooleanField(default=True)
@@ -17,38 +17,44 @@ class Users(models.Model):
     def __unicode__(self):
         return str(self.id) + " " + self.username
 
-class QuizCategories(models.Model):
+class QuizCategory(models.Model):
     category_name = models.CharField(max_length=20, unique=True)
     top_scorer = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True, auto_now_add=True)
-    last_updater = models.ForeignKey(Users)
+    last_updater = models.ForeignKey(User)
+
+    class Meta:
+        verbose_name_plural = "QuizCategories"
 
     def __unicode__(self):
         return str(self.id) + " " + self.category_name
 
-class Quizzes(models.Model):
-    quizcatid = models.ForeignKey(QuizCategories)
+class Quiz(models.Model):
+    quizcatid = models.ForeignKey(QuizCategory)
     quiz_title = models.CharField(max_length=30)
     quiz_desc = models.CharField(max_length=255)
     last_update = models.DateTimeField(auto_now=True, auto_now_add=True)
-    last_updater = models.ForeignKey(Users)
+    last_updater = models.ForeignKey(User)
+
+    class Meta:
+        verbose_name_plural = "Quizzes"
 
     def __unicode__(self):
         return str(self.id) + " " + self.quiz_title
 
-class Tags(models.Model):
+class Tag(models.Model):
     taglabel = models.CharField(max_length=20)
     last_update = models.DateTimeField(auto_now=True, auto_now_add=True)
-    last_updater = models.ForeignKey(Users)
+    last_updater = models.ForeignKey(User)
 
     def __unicode__(self):
         return str(self.id) + " " + self.taglabel
 
-class QuizTags(models.Model):
-    tagid = models.ForeignKey(Tags, primary_key=True)
-    quizid = models.ForeignKey(Quizzes)
+class QuizTag(models.Model):
+    tagid = models.ForeignKey(Tag, primary_key=True)
+    quizid = models.ForeignKey(Quiz)
     last_update = models.DateTimeField(auto_now=True, auto_now_add=True)
-    last_updater = models.ForeignKey(Users)
+    last_updater = models.ForeignKey(User)
     unique_together = ("tagid", "quizid")
 
     def __unicode__(self):
