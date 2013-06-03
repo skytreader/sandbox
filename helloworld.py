be051050ad38c7e63103bef3c091815dd667bfa9
skytreader <chadestioco@gmail.com>
2013-06-03 18:40:06 +0800

    Modified hello world example to distinguish between GET and POST requests.

--- helloworld.py
+++ helloworld.py
@@ -1,9 +1,18 @@
 import tornado.ioloop
 import tornado.web
 
+"""
+TODO:
+ - View templates
+ - Database connection
+"""
+
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("Hello World")
+        self.write("This was from a get method.")
+
+    def post(self):
+        self.write("This is from a post method.")
 
 app = tornado.web.Application([(r"/", MainHandler)])
 
