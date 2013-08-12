d8874db981d797df21911aad7085d857929a7a3f
skytreader <chadestioco@gmail.com>
2013-08-12 18:55:47 +0800

    Source code template.

---
+++ net/skytreader/battleship/NetworkingInterface.java
@@ -0,0 +1,25 @@
+package net.skytreader.battleship;
+
+import java.io.IOException;
+
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+
+import java.nio.ByteBuffer;
+
+import com.sun.nio.sctp.MessageInfo;
+import com.sun.nio.sctp.SctpChannel;
+
+/**
+Networking interface for battleship.
+
+Based on http://sandarenu.blogspot.com/2009/05/sctp-client-server-in-java.html .
+
+@author Chad Estioco
+*/
+public class NetworkingInterface{
+    
+    
+
+}
