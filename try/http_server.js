4c5260b87e7b716c331ff800d96aa4d93311fc42
skytreader <chadestioco@gmail.com>
2013-08-22 18:23:24 +0800

    Pretty basic HTTP Server doing very simple stuff.

---
+++ try/http_server.js
@@ -0,0 +1,20 @@
+var http = require("http");
+var url = require("url");
+
+function handleReq(request, response){
+    console.log("Request received.");
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    
+    // Parse the HTTP request parameters
+    var url_parts = url.parse(request.url, true);
+    var query = url_parts.query;
+
+    response.write("Hello world is a stupid program and should never be used.\n");
+    response.write("Hello " + query["name"]);
+
+    response.end();
+    console.log("Response sent.");
+}
+
+http.createServer(handleReq).listen(8888);
+console.log("Server started.");
