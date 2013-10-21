1a2fbd8ab1e1588631d93050d45b2a405b3ce456
Chad Estioco <chadestioco@gmail.com>
2013-10-22 02:32:33 +0800

    Note on imports in coffee_swing.py .

--- hydra/coffee_swing.py
+++ hydra/coffee_swing.py
@@ -1,5 +1,17 @@
 from javax.swing import JFrame
 
+"""
+Important note:
+
+When using Jython, favor the import form used above. Upon invocation, Jython
+tries to scan for Java packages in your classpath. However, it may fail to find
+some packages for any number of reasons (permissions, configuration, etc.). The
+import form used above is always guaranteed to work.
+
+See also:
+https://wiki.python.org/jython/PackageScanning
+"""
+
 w = JFrame("Jython!")
 w.size = (200, 200)
 w.show()
