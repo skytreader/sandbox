e765988c0940f197a2dcedf465c2558189af2e5e
Chad Estioco <chadestioco@gmail.com>
2013-10-22 16:40:03 +0800

    Simpler SnakeCaller.

--- hydra/SnakeCaller.java
+++ hydra/SnakeCaller.java
@@ -1,13 +1,8 @@
-import java.io.FileInputStream;
-
 import org.python.util.PythonInterpreter;
 
 public class SnakeCaller{
     public static void main(String[] args) throws Exception{
         PythonInterpreter interpreter = new PythonInterpreter();
-        FileInputStream pythonSource = new FileInputStream("pyscript.py");
-        interpreter.execfile(pythonSource);
-
-        pythonSource.close();
+        interpreter.execfile("pyscript.py");
     }
 }
