ebe082bb9da4f59b62c3b10fca3efbe34a00cc6d
Chad Estioco <chadestioco@gmail.com>
2013-10-22 14:02:58 +0800

    (Semi) Proper resource handling in SnakeCaller.java: close FileInputStream after use.

--- hydra/SnakeCaller.java
+++ hydra/SnakeCaller.java
@@ -5,9 +5,9 @@ import org.python.util.PythonInterpreter;
 public class SnakeCaller{
     public static void main(String[] args) throws Exception{
         PythonInterpreter interpreter = new PythonInterpreter();
-        //BufferedReader pythonSource = new BufferedReader(new InputStreamReader(new FileInputStream("pyscript.py")));
-        interpreter.execfile(new FileInputStream("pyscript.py"));
+        FileInputStream pythonSource = new FileInputStream("pyscript.py");
+        interpreter.execfile(pythonSource);
 
-        //pythonSource.close();
+        pythonSource.close();
     }
 }
