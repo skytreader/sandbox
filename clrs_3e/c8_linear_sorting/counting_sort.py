02f3ebc33ffb972c9966492e626d891d93a8553a
Chad Estioco <chadestioco@gmail.com>
2013-10-29 18:56:07 +0800

    Fixed failing test case. Mistake is with the test case itself, not the algorithm.

--- clrs_3e/c8_linear_sorting/counting_sort.py
+++ clrs_3e/c8_linear_sorting/counting_sort.py
@@ -22,7 +22,7 @@ def counting_sort(numlist, range_upper_bound):
     i = 1
 
     # Increment by the number of items less than numlist[i]
-    # Increment limit by 1 because due to indexing concerns
+    # Increment limit by 1 due to indexing concerns
     while i < (range_upper_bound + 1):
         count_list[i] += count_list[i - 1]
         i += 1
@@ -52,7 +52,7 @@ class FunctionsTest(unittest.TestCase):
         zero_open_repeating_sorted = [1, 1, 2, 3, 3, 4, 4, 5, 6, 7, 8, 9]
 
         nine_open_repeating = [5, 1, 3, 7, 1, 3, 4, 2, 0, 4, 8, 6]
-        nine_open_repeating_sorted = [0, 1, 1, 2, 3, 3, 4, 4, 5, 6, 7, 8, 9]
+        nine_open_repeating_sorted = [0, 1, 1, 2, 3, 3, 4, 4, 5, 6, 7, 8]
 
         pi_test = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 9, 3, 2, 3, 8, 4,
             6, 2, 6, 4, 3, 3, 8, 3, 2, 7, 9, 5, 0, 2, 8, 8, 4, 1, 9, 7, 1, 6, 9]
@@ -64,7 +64,7 @@ class FunctionsTest(unittest.TestCase):
         self.assertEqual(counting_sort(nine_open_unique, 9), nine_open_unique_sorted)
         self.assertEqual(counting_sort(zero_open_repeating, 9), zero_open_repeating_sorted)
         self.assertEqual(counting_sort(nine_open_repeating, 9), nine_open_repeating_sorted)
-        self.assertEqual(counting_sort(pi_test, 9), pi_test_sorted)
+        self.assertEqual(counting_sort(pi_test, 9), pi_sorted)
 
 if __name__ == "__main__":
     unittest.main()
