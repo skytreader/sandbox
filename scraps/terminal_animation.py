d7e0cdaa334c0a85763b92de1ea35487004aacb8
Chad Estioco <chadestioco@gmail.com>
2013-11-28 11:39:42 +0800

    Multiline animation hack.

--- scraps/terminal_animation.py
+++ scraps/terminal_animation.py
@@ -1,12 +1,14 @@
+import os
 import random
 import string
 import sys
 import time
 
 def random_string(l):
-    return "".join([random.choice(string.letters) for i in range(l)])
+    return "\n".join([random.choice(string.letters) for i in range(l)])
 
 for i in range(100):
+    os.system("clear")
     sys.stdout.write(random_string(28) + "\r")
     sys.stdout.flush()
     time.sleep(1)
