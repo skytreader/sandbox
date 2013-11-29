749bb54395f18c0ce9bbf380cfb8543a59e9a2cf
Chad Estioco <chadestioco@gmail.com>
2013-11-29 14:27:03 +0800

    Added straight example from the net.

---
+++ fromthenet/boss_worker.py
@@ -0,0 +1,21 @@
+import gevent
+from gevent.queue import Queue
+
+tasks = Queue()
+
+def worker(n):
+    while not tasks.empty():
+        task = tasks.get()
+        print "Worker " + n + " got task " + task
+        gevent.sleep(1)
+    
+    print n + "'s day is done."
+
+def boss():
+    for i in xrange(1, 25):
+        tasks.put_nowait(str(i))
+
+gevent.spawn(boss).join()
+
+gevent.joinall((gevent.spawn(worker, "steve"), gevent.spawn(worker, "bruce"),
+  gevent.spawn(worker, "tony")))
