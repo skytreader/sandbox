f82878b17a0e12623c15f316dbd4a6d2b7fb6fd2
Chad Estioco <chadestioco@gmail.com>
2013-12-18 03:35:16 +0800

    Code simplifications, lots of debugging prints, index adjustments.

--- algorithms/nqueens/NQueens.java
+++ algorithms/nqueens/NQueens.java
@@ -13,38 +13,52 @@ public class NQueens{
     static int[] getNextState(int[] queenConfiguration){
         /*Find the first nonzero cell in queenConfigurations. If there is none.
         return the index of the last cell.*/
-        int backtrackCell = queenConfigurations.length - 1;
+        System.out.println("getNextState: for " + Arrays.toString(queenConfiguration)); 
+        int backtrackCell = queenConfiguration.length - 1;
 
-        for(int i = 0; i < queenConfigurations.length; i++){
-            if(queenConfigurations[i] == 0){
+        for(int i = 0; i < queenConfiguration.length; i++){
+            if(queenConfiguration[i] == 0){
                 backtrackCell = i;
                 break;
             }
         }
 
-        while(queenConfiguration[backtrackCell] <= queenConfiguration.length){
-            if(isValidConfiguration(queenConfiguration)){
-                break;
-            }
+        System.out.println("getNextState: backtrack on index " + backtrackCell);
 
-            queenConfiguration[backtrackCell]++;
+        queenConfiguration[backtrackCell]++;
+
+        System.out.println("getNextState: Entering MEGA loop now...");
+
+        for(int i = 0; i < backtrackCell; i++){
+            while(isCheckingPosition(queenConfiguration[backtrackCell], (backtrackCell + 1),
+              queenConfiguration[i], (i + 1)) && queenConfiguration[backtrackCell] < queenConfiguration.length){
+                    queenConfiguration[backtrackCell]++;
+            }
         }
 
-        return nextConfigurations;
+        System.out.println("getNextState: next state is " + Arrays.toString(queenConfiguration));
+
+        return queenConfiguration;
     }
 
     static boolean isDiagonalChecking(int r1, int c1, int r2, int c2){
+        //System.out.println("isDiagonalChecking: " + r1 + " " + c1 + " " + r2 + " " + c2);
+        //System.out.println("isDiagonalChecking: " + (Math.abs(c2 - c1) == Math.abs(r2 - r1)));
         return Math.abs(c2 - c1) == Math.abs(r2 - r1);
     }
 
     static boolean isCheckingPosition(int r1, int c1, int r2, int c2){
-        return (r1 != 0 && r2 != 0) && ((r1 == r2) || isDiagonalChecking(r1, c1, r2, c2));
+        System.out.println("isCheckingPosition: " + r1 + " " + c1 + " " + r2 + " " + c2);
+        System.out.println("isCheckingPosition: same row? " + (r1 == r2));
+        System.out.println("isCheckingPosition: diagonal checking? " + isDiagonalChecking(r1, c1, r2, c2));
+        return (r1 == r2) || isDiagonalChecking(r1, c1, r2, c2);
     }
 
     static boolean isValidConfiguration(int[] queenConfiguration){
         for(int i = 0; i < queenConfiguration.length; i++){
-            for(int j = i + 1; j < queenConfiguration.length && queenConfiguration[j] != 0; j++){
-                if(isCheckingPosition(queenConfiguration[i], i, queenConfiguration[j], j)){
+            for(int j = i + 1; j < queenConfiguration.length; j++){
+                if(isCheckingPosition(queenConfiguration[i], (i + 1), queenConfiguration[j], (j + 1)) ||
+                  queenConfiguration[j] == 0){
                     return false;
                 }
             }
@@ -97,19 +111,20 @@ public class NQueens{
             int[] queenConfiguration = new int[queenCount];
             Stack<Integer[]> backtracker = new Stack<Integer[]>();
             backtracker.push(autobox(queenConfiguration));
-            //System.out.println("PUSH " + Arrays.toString(queenConfiguration));
+            System.out.println("PUSH " + Arrays.toString(queenConfiguration));
 
             while(!backtracker.empty()){
                 int[] config = autoUnbox(backtracker.pop());
-                //System.out.println("POP " + Arrays.toString(config));
+                System.out.println("POP " + Arrays.toString(config));
                 if(isValidConfiguration(config)){
-                    //System.out.println("Popped a valid configuration!");
+                    System.out.println("Popped a valid configuration!");
                     validCount++;
                 }
 
                 if(canProceed(config)){
                     int[] nextStates = getNextState(config);
-                    backtracker.push(autobox(nextStates[i]));
+                    System.out.println("PUSH: " + Arrays.toString(nextStates));
+                    backtracker.push(autobox(nextStates));
                 }
             }
 
