769f6362beddc577d645cd3c5e516de98dd26955
Chad Estioco <chadestioco@gmail.com>
2013-12-24 04:28:20 +0800

    Fixed problems with queens going off-board.
    But it does not seem to terminate properly yet. Need to check more.

--- algorithms/nqueens/NQueens.java
+++ algorithms/nqueens/NQueens.java
@@ -25,20 +25,32 @@ public class NQueens{
 
         System.out.println("getNextState: backtrack on index " + backtrackCell);
 
+//        if(queenConfiguration[backtrackCell] == queenConfiguration.length){
+//            return null;
+//        }
+
         queenConfiguration[backtrackCell]++;
 
-        System.out.println("getNextState: Entering MEGA loop now...");
+        //System.out.println("getNextState: Entering MEGA loop now...");
 
         for(int i = 0; i < backtrackCell; i++){
             while(isCheckingPosition(queenConfiguration[backtrackCell], (backtrackCell + 1),
-              queenConfiguration[i], (i + 1)) && queenConfiguration[backtrackCell] < queenConfiguration.length){
+              queenConfiguration[i], (i + 1)) && queenConfiguration[backtrackCell] <= queenConfiguration.length){
                     queenConfiguration[backtrackCell]++;
             }
 
-            if(queenConfiguration[backtrackCell] >= queenConfiguration.length){
+            while(queenConfiguration[backtrackCell] > queenConfiguration.length){
+                System.out.println("getNextState: column-wise backtrack.");
                 queenConfiguration[backtrackCell] = 0;
                 backtrackCell--;
-            }
+                if(backtrackCell >= 0){
+                    queenConfiguration[backtrackCell]++;
+                } else{
+                    break;
+                }
+                // To account for the i++ the for loop automagically does.
+                i = -1;
+            } 
         }
 
         System.out.println("getNextState: next state is " + Arrays.toString(queenConfiguration));
@@ -64,9 +76,11 @@ public class NQueens{
     of them threaten another.
     */
     static boolean isValidConfiguration(int[] queenConfiguration){
+        if(queenConfiguration[0] == 0) return false;
         for(int i = 0; i < queenConfiguration.length; i++){
             for(int j = i + 1; j < queenConfiguration.length; j++){
-                if(isCheckingPosition(queenConfiguration[i], (i + 1), queenConfiguration[j], (j + 1))){
+                if(isCheckingPosition(queenConfiguration[i], (i + 1), queenConfiguration[j], (j + 1)) ||
+                  queenConfiguration[j] == 0){
                     return false;
                 }
             }
@@ -131,8 +145,11 @@ public class NQueens{
 
                 if(canProceed(config)){
                     int[] nextStates = getNextState(config);
-                    System.out.println("PUSH: " + Arrays.toString(nextStates));
-                    backtracker.push(autobox(nextStates));
+                    
+                    if(nextStates != null){
+                        System.out.println("PUSH: " + Arrays.toString(nextStates));
+                        backtracker.push(autobox(nextStates));
+                    }
                 }
             }
 
