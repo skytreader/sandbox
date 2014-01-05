d9591c0b9e8d9ec18d8211d97479c3a11dcd2ad8
Chad Estioco <chadestioco@gmail.com>
2014-01-05 18:22:07 +0800

    Added more comments and debouncing code.
    Oddly, after last time, it seems that my code is not yet perfectly working.
    Looks like bouncing. Added some debouncing code but still not flawless.

--- blink/blink_toggle/blink_toggle.ino
+++ blink/blink_toggle/blink_toggle.ino
@@ -1,3 +1,7 @@
+/**
+My own take on making the switch a toggle (and not just a simple circuit breaker).
+*/
+
 #define LED 8
 #define BUTTON 4
 
@@ -12,13 +16,19 @@ void setup(){
 void loop(){
     val = digitalRead(BUTTON);
     
+    // If button was pressed on this turn...
     if(val == HIGH){
         if(toggleFlag == LOW){
+            // LED is off, turn it on.
             toggleFlag = HIGH;
         } else{
+            // LED is on turn it off.
             toggleFlag = LOW;
         }
+        // De-bouncing code, add a 10ms delay.
+        delay(50);
     }
-
+    
+    // And now write it to LED.
     digitalWrite(LED, toggleFlag);
 }
