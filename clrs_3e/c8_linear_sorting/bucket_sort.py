fa300abf2c0a0914b9d35765d9aed1a45a8291a1
Chad Estioco <chadestioco@gmail.com>
2014-01-07 18:34:46 +0800

    Template for bucket sort.

---
+++ clrs_3e/c8_linear_sorting/bucket_sort.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python3
+
+import unittest
+
+"""
+Implementation of bucket sort from CORMEN.
+"""
+
+def insertion_sort(numlist):
+    """
+    Sorts the list using insertion sort. Needed as an auxiliary procedure in
+    bucket_sort.
+    """
+    pass
+
+def bucket_sort(numlist):
+    """
+    Implementation of bucket sort.
+    """
+    pass
