c2ca8fa099e1f8a77c75c13a627e5e09ddfc4afe
Chad Estioco <chadestioco@gmail.com>
2014-01-08 14:24:52 +0800

    Insertion sort implementation for bucket_sort.

--- clrs_3e/c8_linear_sorting/bucket_sort.py
+++ clrs_3e/c8_linear_sorting/bucket_sort.py
@@ -11,7 +11,23 @@ def insertion_sort(numlist):
     Sorts the list using insertion sort. Needed as an auxiliary procedure in
     bucket_sort.
     """
-    pass
+    for i in range(1, len(numlist)):
+        insert(numlist, i)
+
+    return numlist
+
+def insert(numlist, sorted_limit):
+    """
+    sorted_limit is the first index of the unsorted part of the list. So if
+    sorted_limit == len(numlist), the whole list is assumed to be sorted.
+    """
+    if sorted_limit < len(numlist):
+        for i in range(sorted_limit):
+            if numlist[sorted_limit] < numlist[i]:
+                for j in range(i, sorted_limit):
+                    numlist[j], numlist[sorted_limit] = numlist[sorted_limit], numlist[j]
+    
+    return numlist
 
 def bucket_sort(numlist):
     """
