a92631807ba036f0df3f1398579f20969b94133f
Chad Estioco <chadestioco@gmail.com>
2014-01-08 17:38:56 +0800

    Finished implementation of bucket sort.

--- clrs_3e/c8_linear_sorting/bucket_sort.py
+++ clrs_3e/c8_linear_sorting/bucket_sort.py
@@ -1,9 +1,23 @@
 #! /usr/bin/env python3
 
+import math
 import unittest
 
 """
 Implementation of bucket sort from CORMEN.
+
+Algorithm self-description:
+The input is assumed to be numbers in the half-open interval [0, 1). They are
+assumed to be uniformly-distributed.
+
+Bucket sort works by creating n buckets where n is the number of input items.
+This will equally divide the range [0, 1) into smaller ranges. By the assumption
+that the input is uniformly distributed, we place each number into their
+respective bucket. We sort each bucket (shouldn't contain too many items)
+individually and then concatenate their results.
+
+Note that if the uniform distribution assumption does not hold, the algorithm
+degenerates to the runtime of the sorting algorithm we use to sort each bucket.
 """
 
 def insertion_sort(numlist):
@@ -33,4 +47,25 @@ def bucket_sort(numlist):
     """
     Implementation of bucket sort.
     """
-    pass
+    limit = len(numlist)
+    buckets = [[] for i in range(limit)]
+
+    for val in numlist:
+        buckets[math.floor(limit * val)].append(val)
+
+    ordered_list = []
+
+    for b in buckets:
+        insertion_sort(b)
+        ordered_list.extend(b)
+
+    return ordered_list
+
+class FunctionsTest(unittest.TestCase):
+    
+    def test_bucket_sort(self):
+        self.assertEqual(bucket_sort([0.78, 0.17, 0.39, 0.26, 0.72, 0.94, 0.21, 0.12, 0.23, 0.68]),
+          [0.12, 0.17, 0.21, 0.23, 0.26, 0.39, 0.68, 0.72, 0.78, 0.94])
+
+if __name__ == "__main__":
+    unittest.main()
