6ddb02bd8f595b775600b7391c41e7f151cef324
Chad Estioco <chadestioco@gmail.com>
2014-01-21 15:30:16 +0800

    Understood CORMEN now. Adding an unfinished implementation of the method.

--- clrs_3e/c9_medians_and_order_statistics/simul_minmax.py
+++ clrs_3e/c9_medians_and_order_statistics/simul_minmax.py
@@ -2,11 +2,20 @@
 
 import unittest
 
+"""
+Deliberately avoids using Python's built-in min and max functions to explicitly
+show comparisons.
+
+TODO Note how many comparisons are made for each algorithm.
+"""
+
 def simul_minmax(numlist):
     """
     From the given list of numbers, simulatneously pick the minimum and maximum.
     The return value is a tuple pair with the minimum as the first element and
     the maximum as the second element.
+
+    Assumes that list is not empty.
     """
     curmindex = 0
     curmaxdex = 0
@@ -19,6 +28,42 @@ def simul_minmax(numlist):
 
     return (numlist[curmindex], numlist[curmaxdex])
 
+def cormen_simulminmax(numlist):
+    """
+    I figured it out. As described in CORMEN using at most 3*floor(n/2) 
+    comparisons. Returns a tuple pair with the minimum as the first element and
+    the maximum as the second element.
+    """
+    curmindex = 0
+    curmaxdex = 0
+    startdex = 0
+
+    if len(numlist) % 2:
+        startdex = 1
+    else:
+        startdex = 2
+
+        if numlist[0] < numlist[1]:
+            curmindex = 0
+            curmaxdex = 1
+        elif numlist[0] > numlist[1]:
+            curmaxdex = 0
+            curmindex = 1
+        # else they are equal and it does not really matter
+
+    limit = len(numlist)
+
+    while startdex < limit:
+        if numlist[startdex] < numlist[startdex + 1]:
+            if numlist[startdex] < numlist[curmindex]:
+                curmindex = startdex
+
+            if numlist[startdex + 1] > numlist[curmaxdex]:
+                curmaxdex = startdex + 1
+        elif numlist[startdex] > numlist[startdex + 1]:
+            pass #TODO
+            
+
 class FunctionsTest(unittest.TestCase):
     
     def test_simul_minmax(self):
@@ -30,6 +75,10 @@ class FunctionsTest(unittest.TestCase):
 
         even_count = [3, 1, 4, 1, 5, 9]
         self.assertEqual(simul_minmax(even_count), (1, 9))
+
+        self.assertEqual(simul_minmax([1, 2]), (1, 2))
+        self.assertEqual(simul_minmax([2, 1]), (1, 2))
+        self.assertEqual(simul_minmax([1, 1]), (1, 1))
 
 if __name__ == "__main__":
     unittest.main()
