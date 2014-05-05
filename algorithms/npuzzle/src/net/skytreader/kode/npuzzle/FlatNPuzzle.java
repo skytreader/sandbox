c71013de507c3576af8e3a3ee172e8df49e39d55
Chad Estioco <chadestioco@gmail.com>
2014-05-06 01:56:40 +0800

    Addition implementations.
    Naive inversion count and move function.

--- algorithms/npuzzle/src/net/skytreader/kode/npuzzle/FlatNPuzzle.java
+++ algorithms/npuzzle/src/net/skytreader/kode/npuzzle/FlatNPuzzle.java
@@ -45,11 +45,48 @@ public class FlatNPuzzle implements NPuzzle{
         int col = index % size;
         return new Point(row, col);
     }
+    
+    /**
+    O(n^2) and you can definitely do better.
+    */
+    private int inversionCount(){
+        int inversionCount = 0;
+
+        for(int i = 0; i < size; i++){
+            for(int j = i + 1; j < size; j++){
+                if(puzzle[i] > puzzle[j]){
+                    inversionCount++;
+                }
+            }
+        }
+
+        return inversionCount;
+    }
 
     public void initialize(){
     }
 
     public void move(Direction d){
+        // Find the blank tile
+        int blankIndex = 0;
+        for(; blankIndex < size; blankIndex++){
+            if(puzzle[blankIndex] == 0){
+                break;
+            }
+        }
+
+        //TODO If loop stops and we haven't found any blank tile, raise exception.
+
+        Point p = translateIndex(blankIndex)
+        int newRow = p.x - p.drow();
+        int newCol = p.y - p.dcol();
+
+        int moveIndex = translateRowCol(newRow, newCol);
+
+        // Swap blankIndex and moveIndex
+        int temp = puzzle[moveIndex];
+        puzzle[moveIndex] = puzzle[blankIndex];
+        puzzle[blankIndex] = temp;
     }
 
     public int getTileAt(int row, int col){
