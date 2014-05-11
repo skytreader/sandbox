cd981208dd58a3967336c121463ff4e71e0fe27b
Chad Estioco <chadestioco@gmail.com>
2014-05-11 16:43:02 +0800

    Preliminary tests. Not yet compiling.

--- algorithms/npuzzle/src/net/skytreader/kode/npuzzle/tests/FlatNPuzzleTests.java
+++ algorithms/npuzzle/src/net/skytreader/kode/npuzzle/tests/FlatNPuzzleTests.java
@@ -1,6 +1,7 @@
 package net.skytreader.kode.npuzzle.tests;
 
 import net.skytreader.kode.npuzzle.FlatNPuzzle;
+import net.skytreader.kode.npuzzle.NPuzzle;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -23,5 +24,24 @@ public class FlatNPuzzleTests{
 
         String foo = "Just a random Object for comparison.";
         Assert.assertFalse(fnp.equals(foo));
+
+        // Test reflexivity
+        Assert.assertTrue(fnp.equals(fnp));
+
+        // Create another instance, should still be equal at this point.
+        FlatNPuzzle another = new FlatNPuzzle();
+        Assert.assertTrue(fnp.equals(another));
+        // Test symmetry.
+        Assert.assertTrue(another.equals(fnp));
+
+        // Test transitivity
+        FlatNPuzzle transitiveOther = new FlatNPuzzle();
+        Assert.assertTrue(another.equals(transitiveOther));
+        Assert.assertTrue(fnp.equals(transitiveOther));
+
+        // Move stuff around in another
+        another.move(NPuzzle.Direction.DOWN);
+        Assert.assertFalse(fnp.equals(another));
+        Assert.assertFalse(another.equals(fnp));
     }
 }
