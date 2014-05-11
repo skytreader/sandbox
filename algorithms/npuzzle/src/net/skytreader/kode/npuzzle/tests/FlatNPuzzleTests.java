b77d6d0671cc6e7c5a496d0880a565ab14b6deef
Chad Estioco <chadestioco@gmail.com>
2014-05-11 17:13:13 +0800

    Test compilable but not yet successful.

--- algorithms/npuzzle/src/net/skytreader/kode/npuzzle/tests/FlatNPuzzleTests.java
+++ algorithms/npuzzle/src/net/skytreader/kode/npuzzle/tests/FlatNPuzzleTests.java
@@ -5,13 +5,15 @@ import net.skytreader.kode.npuzzle.NPuzzle;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.Before;
 
 public class FlatNPuzzleTests{
     private FlatNPuzzle fnp;
+    public static final int TEST_SIZE = 4;
 
     @Before
     public void setUp(){
-        fnp = new FlatNPuzzle();
+        fnp = new FlatNPuzzle(FlatNPuzzleTests.TEST_SIZE);
     }
 
     @Test
@@ -29,13 +31,13 @@ public class FlatNPuzzleTests{
         Assert.assertTrue(fnp.equals(fnp));
 
         // Create another instance, should still be equal at this point.
-        FlatNPuzzle another = new FlatNPuzzle();
+        FlatNPuzzle another = new FlatNPuzzle(FlatNPuzzleTests.TEST_SIZE);
         Assert.assertTrue(fnp.equals(another));
         // Test symmetry.
         Assert.assertTrue(another.equals(fnp));
 
         // Test transitivity
-        FlatNPuzzle transitiveOther = new FlatNPuzzle();
+        FlatNPuzzle transitiveOther = new FlatNPuzzle(FlatNPuzzleTests.TEST_SIZE);
         Assert.assertTrue(another.equals(transitiveOther));
         Assert.assertTrue(fnp.equals(transitiveOther));
 
