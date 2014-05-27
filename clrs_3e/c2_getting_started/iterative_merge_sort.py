7d10e2e31cd7a56c4a7bcd798ee476961a7aa261
Chad Estioco <chadestioco@gmail.com>
2014-05-27 14:56:04 +0800

    Added extra "last pass" processing to merge function.
    Going back to iterative merge sort since inversion is not yet working, discovering
    a few bugs.
    
    If skip_count does not divide the list evenly, the last len(l) % skip_count
    items are not being merged. This is already covered in an existing test case but
    it succeeds since the largest item in the set is the one at the last position.

--- clrs_3e/c2_getting_started/iterative_merge_sort.py
+++ clrs_3e/c2_getting_started/iterative_merge_sort.py
@@ -8,11 +8,15 @@ def merge_by(numlist, skip_count):
     while i < limit:
         # list 1 is the range [i, i + skip_count]
         # list 2 is the range [i + skip_count + 1, i + 2 * skip_count]
-        l1 = numlist[i:i + skip_count + 1]
-        l2 = numlist[i + skip_count + 1:i + (2 * skip_count) + 1]
+        l1 = numlist[i:i + skip_count]
+        l2 = numlist[i + skip_count:i + (2 * skip_count)]
+        print("Skip count " + str(skip_count))
+        print(numlist)
+        print(str(l1))
+        print(str(l2))
 
         j = i
-        sublimit = i + (2 * skip_count) + 1
+        sublimit = i + (2 * skip_count)
 
         while j < sublimit and len(l1) and len(l2):
             if l1[0] <= l2[0]:
@@ -31,29 +35,60 @@ def merge_by(numlist, skip_count):
             j += 1
 
         i = sublimit
+        print("===")
+
+    leftover = limit % skip_count
+
+    if leftover:
+        # merge the leftover with the rest of the sorted part
+        cutpoint = limit - leftover
+        l1 = numlist[0:cutpoint]
+        l2 = numlist[cutpoint:limit]
+        print("Last pass")
+        print(l1)
+        print(l2)
+
+        i = 0
+        while i < limit and len(l1) and len(l2):
+            if l1[0] < l2[0]:
+                numlist[i] = l1.pop(0)
+            else:
+                numlist[i] = l2.pop(0)
+
+            i += 1
+        
+        # Since we are sure that l1 is longer than l2
+        while i < limit and len(l1):
+            numlist[i] = l1.pop(0)
+            i += 1
 
 def merge_sort(numlist):
     """
     Keep merging til a skip count of half the numlist size.
     """
-    limit = math.floor(len(numlist) / 2) + 1
+    limit = math.floor(len(numlist) / 2)
     
     for i in range(limit):
-        merge_by(numlist, i)
+        merge_by(numlist, i + 1)
 
 
 class FunctionsTest(unittest.TestCase):
     
     def test_sort(self):
         one = [1]
-        merge_sort(one)
-        self.assertEqual(one, [1])
+        #merge_sort(one)
+        #self.assertEqual(one, [1])
         pi = [1, 4, 1 ,5 ,9, 2]
         merge_sort(pi)
         self.assertEqual(pi, [1, 1, 2, 4, 5, 9])
         pi_odd = [1, 4, 1, 5, 9]
         merge_sort(pi_odd)
         self.assertEqual(pi_odd, [1, 1, 4, 5, 9])
+
+        inversion_case = [2, 3, 8, 6, 1]
+        sorted_inversion_case = sorted(inversion_case)
+        merge_sort(inversion_case)
+        self.assertEqual(sorted_inversion_case, inversion_case)
 
 if __name__ == "__main__":
     unittest.main()
