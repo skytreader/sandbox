b13a7200179c6403378e25eaaf7d95512dfb18e3
Chad Estioco <chadestioco@gmail.com>
2014-06-23 01:57:33 +0800

    Connected-components labeling set-up.

---
+++ algorithms/ccl/src/net/skytreader/kode/cll/LabelMaker.java
@@ -0,0 +1,22 @@
+package net.skytreader.kode.cll;
+
+/**
+An implementation of connected-components labeling, but we should be able to
+use this on a grid with more than two distinct cells. Input, for now, is expected
+to be a two-dimensional array of chars.
+
+This also allows you to query the region to which a given cell belongs to. It
+is recommended that you assign one grid to one instance of LabelMaker.
+
+@author Chad Estioco
+*/
+
+public class LabelMaker{
+    
+    private char[][] grid;
+
+    public LabelMaker(char[][] grid){
+        this.grid = grid;
+    }
+
+}
---
+++ algorithms/ccl/src/net/skytreader/kode/cll/tests/LabelMaker.java
@@ -0,0 +1,9 @@
+package net.skytreader.kode.cll.tests;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.Before;
+
+public class CLLTest{
+    
+}
