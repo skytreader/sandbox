c28788305e73ae600e2c3463c8282eb00a2fe728
Chad Estioco <chadestioco@gmail.com>
2014-06-25 18:40:52 +0800

    Some javadocs and method prototypes.

--- algorithms/ccl/src/net/skytreader/kode/ccl/LabelMaker.java
+++ algorithms/ccl/src/net/skytreader/kode/ccl/LabelMaker.java
@@ -18,9 +18,37 @@ is recommended that you assign one grid to one instance of LabelMaker.
 public class LabelMaker{
     
     private char[][] grid;
+    /**
+    Signals the connectivity style of this instance. True means it is 8-connected
+    otherwise it is 4-connected.
+    */
+    private boolean is8conn;
 
-    public LabelMaker(char[][] grid){
+    public LabelMaker(char[][] grid, boolean is8conn){
         this.grid = grid;
+        this.is8conn = is8conn;
+    }
+    
+    /**
+    This actually performs connected-component labeling algorithm.
+    */
+    private void label(){
+    }
+    
+    /**
+    Returns the neighbors of the cell described in row, col as an array. The
+    neighbors are enumerated in row-major order (left-to-right, top-to-bottom).
+    */
+    private Point[] getNeighbors(int row, int col){
+        if(is8conn){
+            Point[] neighbors;
+
+            return neighbors;
+        } else{
+            Point[] neighbors;
+
+            return neighbors;
+        }
     }
 
     /**
