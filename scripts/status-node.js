1de619dc83b9315ce168db1034e38d93448e46ba
Chad Estioco <chadestioco@gmail.com>
2015-03-02 17:17:04 +0800

    Bells and whistles.

---
+++ scripts/status-node.js
@@ -0,0 +1,14 @@
+var dater = new Date();
+
+function getTimestamp(){
+    return dater.getDate() + "/" + (dater.getMonth() + 1) + "/" + dater.getFullYear() +
+      " " + dater.getHours() + ":" + dater.getMinutes() + ":" + dater.getSeconds();
+}
+
+$(document).ready(function(){
+    $("#post-status").click(function(){
+        var statusPost = document.createElement("div");
+        statusPost.innerHTML = getTimestamp() + " " + $("#user-status").val();
+        $("#status-area").prepend(statusPost);
+    });
+});
