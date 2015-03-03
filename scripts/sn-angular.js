eeaf92f32a6f14c09dc3afa4ef9d53b55f731f59
Chad Estioco <chadestioco@gmail.com>
2015-03-03 14:51:24 +0800

    Angularizing stuff.

---
+++ scripts/sn-angular.js
@@ -0,0 +1,10 @@
+angular.module("statusNodeModule", []).controller("statusNodeController", function($scope){
+    $scope.statusUpdates = [
+        {"timestamp": "2/9/15 12:00:00",
+         "update": "Happy Birthday!!!"},
+        {"timestamp": "1/28/15 12:00:00",
+         "update": "Happy Birthday J!!!"},
+        {"timestamp": "12/4/14 12:00:00",
+         "update": "Happy Birthday S!!!"}
+    ]
+});
