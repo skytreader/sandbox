a2ac9eed3bb8dba1d3d075e69f2d9d197ddab802
Chad Estioco <chadestioco@gmail.com>
2015-10-15 21:31:50 +0800

    More experiments for Kalibrr's navbar.

--- scripts/sn-angular.js
+++ scripts/sn-angular.js
@@ -7,6 +7,24 @@ var qotdModule = angular.module("qotdModule", [])
 
 angular.module("statusNodeApp", ["qotdModule"])
 .controller("statusNodeController", ["$scope", "qotdGetter", function($scope, qotdGetter){
+    this.spam = "Embrr";
+    this.flag = false;
+
+    this.flagTrue = function(){
+        this.flag = true;
+        console.log("flag is", this.flag);
+    }
+
+    this.flagFalse = function(){
+        this.flag = false;
+        console.log("flag is", this.flag);
+    }
+
+    this.getFlag = function(){
+        console.log("returning flag", this.flag);
+        return this.flag;
+    }
+
     $scope.statusUpdates = [
         {"timestamp": "2/9/15 12:00:00",
          "update": "Happy Birthday!!!"},
