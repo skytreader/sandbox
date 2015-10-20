c4822e8a34d4f0f15860c2f4145a337a7b297568
Chad Estioco <chadestioco@gmail.com>
2015-10-20 23:39:53 +0800

    Make tests fail.

--- algorithms/npuzzle/src/test/java/net/skytreader/kode/npuzzle/utils/AlgorithmsTest.java
+++ algorithms/npuzzle/src/test/java/net/skytreader/kode/npuzzle/utils/AlgorithmsTest.java
@@ -35,5 +35,14 @@ public class AlgorithmsTest{
          evenWidthSolvable.setConfig(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
            11, 12, 15, 13, 14, 0});
          Assert.assertTrue(Algorithms.isSolvable(evenWidthSolvable));
+
+         NPuzzle oddWidthUnsolvable = new FlatNPuzzle(3);
+         oddWidthUnsolvable.setConfig(new int[]{0, 1, 2, 3, 4, 5, 6, 8, 7});
+         Assert.assertFalse(Algorithms.isSolvable(oddWidthUnsolvable));
+
+         NPuzzle evenWidthUnsolvable = new FlatNPuzzle(4);
+         evenWidthUnsolvable.setConfig(new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+           10, 11, 12, 13, 14, 15});
+         Assert.assertFalse(Algorithms.isSolvable(evenWidthUnsolvable));
     }
 }
